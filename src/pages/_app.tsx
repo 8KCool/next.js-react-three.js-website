bf79a70a25f7f5b003c39cacb782c805aa755048
PRANTA Dutta <prantadutta1997@gmail.com>
2022-03-11 20:33:19 +0600

    fixed favicon etc.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, maximum-scale=1.0, user-scalable=1"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,28 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -13,6 +35,12 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&family=Sarabun:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
+        />
+
+        <script
+          defer
+          data-domain="trigan.org"
+          src="https://stats.trigan.org/js/plausible.js"
         />
       </Head>
       <body className="bg-dark text-white">
