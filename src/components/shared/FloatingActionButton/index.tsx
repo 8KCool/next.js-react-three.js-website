0400ac06c106ee098b4caff8e98226df4d312b82
PRANTA Dutta <prantadutta1997@gmail.com>
2022-03-18 18:01:29 +0600

    made the button clickable

--- src/components/shared/FloatingActionButton/index.tsx
+++ src/components/shared/FloatingActionButton/index.tsx
@@ -7,10 +7,15 @@ interface FloatingActionButtonProps {
 
 export const FloatingActionButton: React.FC<FloatingActionButtonProps> = () => {
   return (
-    <button className="fixed right-8 z-30 flex h-full w-full flex-row-reverse flex-wrap-reverse">
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://discord.gg/GQ3JyBdg"
+      className="fixed right-8 z-30 flex h-full w-full flex-row-reverse flex-wrap-reverse"
+    >
       <div className="absolute bottom-8 flex h-14 w-14 items-center justify-center rounded-full bg-primary">
         <FaDiscord className="text-2xl" />
       </div>
-    </button>
+    </a>
   )
 }
