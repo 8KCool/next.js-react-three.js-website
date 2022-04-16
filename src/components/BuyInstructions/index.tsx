9d1cabd3671f9c4ff846e4032c4598d3de3d210e
PRANTA Dutta <prantadutta1997@gmail.com>
2022-04-16 12:34:00 +0600

    re-designed the buy instructions

--- src/components/BuyInstructions/index.tsx
+++ src/components/BuyInstructions/index.tsx
@@ -1,123 +1,43 @@
-import { motion } from 'framer-motion'
 import { ReactNode, useState } from 'react'
-import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
-import { FadeInWhenVisible } from '../shared/FadeInWhenVisible'
+import {
+  CHINESE_METAMASK_INSTRUCTIONS,
+  CHINESE_WEBSITE_INSTRUCTIONS,
+  ENGLISH_METAMASK_INSTRUCTIONS,
+  ENGLISH_WEBSITE_INSTRUCTIONS,
+} from './constants'
+import { ShowInstructions } from './ShowInstructions'
 
 interface BuyInstructionsProps {
   children?: ReactNode
 }
 
 export const BuyInstructions: React.FC<BuyInstructionsProps> = () => {
-  const [expandMetamask, setExpandMetamask] = useState(false)
-  const [expandWebsite, setExpandWebsite] = useState(false)
+  const [lang, setLang] = useState<'en' | 'cn'>('en')
+  const metamaskInstructions =
+    lang === 'en'
+      ? ENGLISH_METAMASK_INSTRUCTIONS
+      : CHINESE_METAMASK_INSTRUCTIONS
+  const websiteInstructions =
+    lang === 'en' ? ENGLISH_WEBSITE_INSTRUCTIONS : CHINESE_WEBSITE_INSTRUCTIONS
   return (
-    <div className="mx-auto max-w-md lg:max-w-2xl">
+    <div className="mx-auto">
       {/* How to Buy Trigan Tokens (TED) Via the Metamask Wallet */}
-      <div className="my-5">
-        <button
-          onClick={() => setExpandMetamask(!expandMetamask)}
-          className="flex items-center gap-5 text-xl font-semibold"
-        >
-          {expandMetamask ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />} How
-          to Buy Trigan Tokens (TED) Via the Metamask Wallet
-        </button>
-        {expandMetamask && (
-          <FadeInWhenVisible>
-            <motion.ul
-              initial={{ x: '-100%' }}
-              animate={{ x: 0 }}
-              className="mt-2 ml-10"
-            >
-              <li>
-                1. First thing is to add the Binance smart chain Mainnet to your
-                metamask wallet (You can find the guide{' '}
-                <a
-                  target="_blank"
-                  className="text-primary underline"
-                  rel="noreferrer"
-                  href="https://academy.binance.com/en/articles/connecting-metamask-to-binance-smart-chain"
-                >
-                  here
-                </a>
-                )
-              </li>
-              <li>2. Select the smart chain Mainnet</li>
-              <li>
-                3. Go to{' '}
-                <a
-                  target="_blank"
-                  className="text-primary underline"
-                  rel="noreferrer"
-                  href="https://trigan.org/buy"
-                >
-                  https://trigan.org/buy
-                </a>{' '}
-                on your Metamask DAPP Browser
-              </li>
-              <li>
-                4. Click on Authorize Wallet address (f you can&#39;t authorize
-                wallet, kindly use the contact section below on the site to
-                submit your BSC wallet address for it to be added to the system)
-              </li>
-              <li>
-                5. Once your wallet has been authorized, select how much worth
-                of TED tokens you want to buy
-              </li>
-              <li>6. Click on Buy now</li>
-              <li>7. Congratulations on buying TED</li>
-            </motion.ul>
-          </FadeInWhenVisible>
-        )}
-      </div>
+      <ShowInstructions
+        instructions={metamaskInstructions}
+        lang={lang}
+        title="How to Buy Trigan Tokens (TED) Via the Metamask Wallet"
+        onLangChange={setLang}
+        shortTitle="How to Buy (Metatask)"
+      />
 
       {/* How to Buy Trigan Tokens (TED) Via it’s Website */}
-      <div className="my-5">
-        <button
-          onClick={() => setExpandWebsite(!expandWebsite)}
-          className="flex items-center gap-5 text-xl font-semibold"
-        >
-          {expandWebsite ? <AiOutlineArrowDown /> : <AiOutlineArrowUp />} How to
-          Buy Trigan Tokens (TED) Via it&#39;s Website
-        </button>
-        {expandWebsite && (
-          <FadeInWhenVisible>
-            <motion.ul
-              initial={{ x: '-100%' }}
-              animate={{ x: 0 }}
-              className="mt-2 ml-10"
-            >
-              <li>
-                1. Go to{' '}
-                <a
-                  target="_blank"
-                  className="text-primary underline"
-                  rel="noreferrer"
-                  href="https://trigan.org/buy"
-                >
-                  https://trigan.org/buy
-                </a>{' '}
-                on your Browser
-              </li>
-              <li>2. Click on Buy now</li>
-              <li>
-                3. This will take you to a page with a QR scanner, scan to
-                connect your wallet or manually connect your wallet
-              </li>
-              <li>
-                4. Click on Authorize Wallet address (f you can&#39;t authorize
-                wallet, kindly use the contact section below on the site to
-                submit your BSC wallet address for it to be added to the system)
-              </li>
-              <li>
-                5. Once your wallet has been authorized, select how much worth
-                of TED tokens you want to buy
-              </li>
-              <li>6. Click on Buy now</li>
-              <li>7. Congratulations on buying TED</li>
-            </motion.ul>
-          </FadeInWhenVisible>
-        )}
-      </div>
+      <ShowInstructions
+        instructions={websiteInstructions}
+        lang={lang}
+        title="How to Buy Trigan Tokens (TED) Via it's Website"
+        onLangChange={setLang}
+        shortTitle="How to Buy (Website)"
+      />
     </div>
   )
 }
