f0339198b1244e4036b2ee3a5c475356ae13c133
Robson Melo de Souza <sys.robson@gmail.com>
2022-05-16 11:38:54 -0300

    Advisors withe details included on the Advisors section, advisors category filtered out from the category selector

--- src/components/home/AdvisorsSection/index.tsx
+++ src/components/home/AdvisorsSection/index.tsx
@@ -3,9 +3,9 @@ import { useRouter } from 'next/router'
 import { ReactNode, useState } from 'react'
 import { groupByCategory } from '../../../util/functions'
 import { FadeInWhenVisible } from '../../shared/FadeInWhenVisible'
-import { TeamCatSelector } from '../../shared/TeamCatSelector'
 import { Title } from '../../shared/Title'
 import { TeamMember } from './../../../types/TeamMember'
+import { Advisors } from '../../Teams/Advisors'
 
 interface AdvisorsSectionProps {
   children?: ReactNode
@@ -17,46 +17,11 @@ export const AdvisorsSection: React.FC<AdvisorsSectionProps> = ({ teams }) => {
   const [category, setCategory] = useState('all')
   const teamMembers = groupByCategory(teams, category)
   return (
-    <section id="advisors" className="w-full px-1">
-      <Title title="Board of Advisors" />
-
-      <div className="flex w-full flex-wrap justify-center pb-5 text-center md:px-5">
-        {teamMembers.map((teamMember, i) => {
-          if (teamMember.category == 'Advisors') {
-            return (
-              <div
-                key={teamMember.id}
-                onClick={() => router.push('/teams#' + teamMember.id)}
-                className="w-1/2 cursor-pointer gap-4 p-5 sm:w-1/3 xl:w-1/6 md2:w-1/5"
-              >
-                <FadeInWhenVisible key={teamMember.id} duration={(i + 1) * 0.2}>
-                  <div className="relative mx-auto my-2 h-20 w-20">
-                    {teamMember.image && (
-                      <Image
-                        src={teamMember.image}
-                        alt={teamMember.name}
-                        layout="fill"
-                        className="rounded-full bg-light"
-                      />
-                    )}
-                  </div>
-
-                  <h2 className="text-lg md:whitespace-nowrap">
-                    {teamMember.name}
-                  </h2>
-                  <p className="text-medium">{teamMember.title}</p>
-                </FadeInWhenVisible>
-              </div>
-            )
-          }
-        })}
-      </div>
-      <button
-        onClick={() => router.push('/advisorsBoard')}
-        className="primary-btn"
-      >
-        Learn More
-      </button>
-    </section>
+    <div className="flex w-full flex-wrap justify-center pb-5 text-center md:px-5">
+      <Title padding="py-3" title="Meet Our Advisors" />
+      <section id="advisors" className="w-full px-1">
+        <Advisors key={category} teams={groupByCategory(teams, category)} />
+      </section>
+    </div>
   )
 }
