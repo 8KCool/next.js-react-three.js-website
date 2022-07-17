fdf0b7a908acfa585f22bf3f51bdb88ba595f74e
Robson Melo de Souza <sys.robson@gmail.com>
2022-07-17 08:52:32 -0300

    Project Hero redesigned

---
+++ src/pages/ProjectHero/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+import { ProjectSection } from '../../components/home/ProjectSection'
+import { GlobalLayout } from '../../components/layouts/GlobalLayout'
+
+interface ProjectHeroProps {
+  children?: ReactNode
+}
+
+const ProjectHero: React.FC<ProjectHeroProps> = () => {
+  return (
+    <GlobalLayout>
+      <ProjectSection />
+    </GlobalLayout>
+  )
+}
+
+export default ProjectHero
