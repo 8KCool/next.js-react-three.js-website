053edd4ac57c2fb06c534686ef37efebb036ff9a
programmerbanna <programmerbanna@gmail.com>
2022-10-16 20:27:52 +0600

    re updated for free money for all

--- src/components/home/HeroSection/Projects.tsx
+++ src/components/home/HeroSection/Projects.tsx
@@ -9,7 +9,7 @@ interface HeroSectionProps {
 
 export const HeroSection: React.FC<HeroSectionProps> = ({ project }) => {
   return (
-    <header className="relative grid items-center bg-primary bg-opacity-70">
+    <header className="relative grid items-center bg-slate-300 bg-opacity-70">
       <video
         className="absolute object-cover w-auto h-full min-w-full -z-10"
         width="618"
@@ -22,21 +22,21 @@ export const HeroSection: React.FC<HeroSectionProps> = ({ project }) => {
         <source src="/videos/trigan nation loop hero.mp4" type="video/mp4" />
       </video>
       <div className="grid items-center justify-center grid-cols-1 text-center py-36 xl:py-48 2xl:py-20 2xl:pt-48 ">
-        <div className="flex flex-col justify-center px-2 mx-auto text-white sm:px-5 2xl:w-1/2 2xl:py-6">
+        <div className="flex flex-col justify-center px-2 mx-auto text-black sm:px-5 2xl:w-1/2 2xl:py-6">
           <motion.h2
             initial={{ x: '-800px' }}
             animate={{ x: 0 }}
             transition={{ duration: 0.3 }}
             className=" text-sans"
           >
-            <div className="text-4xl font-extralight md:text-6xl lg:text-7xl 2xl:py-5">
+            <div className="text-4xl headingStyle md:text-6xl lg:text-7xl 2xl:py-5">
               {project.name}
             </div>
-            <div className="whitespace-normal text-4xl md:mt-[-12px] md:text-6xl lg:mt-[-16px] lg:text-7xl 2xl:mt-[-36px]">
+            <div className="headingStyle whitespace-normal text-4xl md:mt-[6px] md:text-6xl lg:mt-[16px] lg:text-7xl 2xl:mt-[-12px]">
               {project.description}
             </div>
             <div className="mx-auto w-[90%] font-extralight sm:w-[80%] sm:px-14 md:text-2xl lg:w-[45%] 2xl:w-[70%] 2xl:px-0 2xl:text-3xl">
-              <p className="py-4">{project.content}</p>
+              <p className="py-4 paragraphStyle">{project.content}</p>
             </div>
           </motion.h2>
         </div>
