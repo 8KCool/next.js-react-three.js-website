5aba964b71e123f1070278fafd7fa92d37dd0c1f
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-03 23:01:36 +0100

    Pushing the changes to improvements branch

--- src/components/home/HeroSection/index.tsx
+++ src/components/home/HeroSection/index.tsx
@@ -1,5 +1,17 @@
-import { motion } from 'framer-motion'
 import { ReactNode } from 'react'
+import Image from 'next/image'
+
+import { motion } from 'framer-motion'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// import required modules
+import { Pagination } from 'swiper'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 interface HeroSectionProps {
   children?: ReactNode
@@ -14,34 +26,34 @@ const BUTTONS = [
 
 export const HeroSection: React.FC<HeroSectionProps> = () => {
   return (
-    <header className="relative grid items-center bg-slate-300 bg-opacity-70">
+    <header className="relative grid bg-slate-300/70">
       <video
-        className="absolute object-cover w-auto h-full min-w-full -z-10"
+        className="fixed -z-10 h-full w-auto min-w-full object-cover"
         width="618"
         height="347"
-        autoPlay
         loop
         muted
         preload="auto"
+        id="myVid"
       >
-        <source src="/videos/bg-video.mp4" type="video/mp4" />
+        <source src="/videos/trigan-bg-720.mp4" type="video/mp4" />
       </video>
-      <div className="grid items-center justify-center grid-cols-1 text-center py-36 xl:py-48 2xl:py-20 2xl:pt-48 ">
-        <div className="flex flex-col justify-center px-2 mx-auto text-black sm:px-5 2xl:w-1/2 2xl:py-6">
-          <motion.h2
+      {/* <div className="grid grid-cols-1 items-center justify-center py-36 text-center xl:py-48 2xl:py-20 2xl:pt-48 "> */}
+      {/* <div className="mx-auto flex flex-col justify-center px-2 text-black sm:px-5 2xl:w-1/2 2xl:py-6"> */}
+      {/* <motion.h2
             initial={{ x: '-800px' }}
             animate={{ x: 0 }}
             transition={{ duration: 0.3 }}
-            className=" text-sans"
+            className="text-sans"
           >
-            <div className="text-4xl headingStyle md:text-6xl lg:text-7xl 2xl:py-5">
+            <div className="headingStyle text-4xl md:text-6xl lg:text-7xl 2xl:py-5">
               Real World Utopia.
             </div>
             <div className="headingStyle whitespace-normal text-4xl md:mt-[6px] md:text-6xl lg:mt-[16px] lg:text-7xl 2xl:mt-[-12px]">
               Beyond Metaverse
             </div>
             <div className="mx-auto w-[90%] font-extralight sm:w-[80%] sm:px-14 md:text-2xl lg:w-[45%] 2xl:w-[70%] 2xl:px-0 2xl:text-3xl">
-              <p className="py-4 paragraphStyle">
+              <p className="paragraphStyle py-4">
                 Trigan is a revolutionary start-up based in Scotland with a
                 global team and one shared vision;
               </p>
@@ -50,16 +62,16 @@ export const HeroSection: React.FC<HeroSectionProps> = () => {
                 supporting development, growth and empowerment.
               </p>
             </div>
-            <p className="pt-4 text-2xl font-extrabold md:text-md 2xl:pt-6">
+            <p className="md:text-md pt-4 text-2xl font-extrabold 2xl:pt-6">
               Opportunity
             </p>
-            <p className="text-sm md:text-md paragraphStyle xl:text-2xl">
+            <p className="md:text-md paragraphStyle text-sm xl:text-2xl">
               Pre-sale now launched.
             </p>
-          </motion.h2>
-          {/* Button Starts */}
-          <div className="justify-start mt-4 text-white">
-            {BUTTONS.map((button, i) => {
+          </motion.h2> */}
+      {/* Button Starts */}
+      {/* <div className="mt-4 justify-start text-white"> */}
+      {/* {BUTTONS.map((button, i) => {
               return (
                 <motion.a
                   initial={{ x: '-800px' }}
@@ -76,15 +88,15 @@ export const HeroSection: React.FC<HeroSectionProps> = () => {
                   {button.title}
                 </motion.a>
               )
-            })}
-          </div>
-          {/* Button Ends */}
-        </div>
+            })} */}
+      {/* </div> */}
+      {/* Button Ends */}
+      {/* </div> */}
 
-        {/* Video Starts */}
-        {/* <div className="relative p-5 opacity-100 md:p-10 lg:p-20"> */}
-        {/* <AnimationBlob /> */}
-        {/* <div className="flex justify-center aspect-w-16 aspect-h-9">
+      {/* Video Starts */}
+      {/* <div className="relative p-5 opacity-100 md:p-10 lg:p-20"> */}
+      {/* <AnimationBlob /> */}
+      {/* <div className="flex justify-center aspect-w-16 aspect-h-9">
             <iframe
               allowFullScreen
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -92,8 +104,130 @@ export const HeroSection: React.FC<HeroSectionProps> = () => {
               src="https://www.youtube.com/embed/YYAZ1mnz3bM?mode=opaque&amp;rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent"
             />
           </div> */}
-        {/* </div> */}
-        {/* Video Ends */}
+      {/* </div> */}
+      {/* Video Ends */}
+      {/* </div> */}
+
+      <div className="mx-auto max-w-5xl py-28 text-center xl:py-48 2xl:py-20 2xl:pt-48">
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination]}
+          className="heroSwiper"
+        >
+          <SwiperSlide className="w-full">
+            <div className="w-full">
+              <div className="font-sarabun">
+                <h2 className="mb-2 text-xl font-semibold text-black md:text-5xl">
+                  <span
+                    style={{ fontStyle: 'italic' }}
+                    className="font-roboto font-light"
+                  >
+                    Trigan
+                  </span>{' '}
+                  - Driving social change through technological evolution
+                </h2>
+
+                <p className="text-lg font-medium md:py-2 md:text-xl xl:px-36">
+                  Trigan’s unique blockchain technology enables a fair,
+                  transparent, and decentralised new economy for the benefit of
+                  everyone, everywhere.
+                </p>
+
+                <div className="">
+                  <Image src={''} className="" alt="Image Goes here" />
+                </div>
+              </div>
+              <button className="mb-6 -mt-2 rounded-full bg-primary px-8 py-4 font-medium text-slate-100">
+                The first urban blockchain
+              </button>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide className="w-full">
+            <div className="w-full">
+              <div className="font-sarabun">
+                <h2 className="mb-2 text-xl font-semibold text-black md:text-5xl">
+                  <span
+                    style={{ fontStyle: 'italic' }}
+                    className="font-roboto font-light"
+                  >
+                    Trigan
+                  </span>{' '}
+                  - Communities
+                </h2>
+
+                <p className="text-lg font-medium md:py-2 md:text-xl xl:px-36">
+                  The smart city operating system for real-world communities,
+                  improving quality of life in towns and cities on Earth.
+                </p>
+
+                <div className="">
+                  <Image src={''} className="" alt="Image Goes here" />
+                </div>
+              </div>
+              <button className="rounded-full bg-primary px-8 py-4 font-medium text-slate-100">
+                Lorem ipsum
+              </button>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide className="w-full">
+            <div className="w-full">
+              <div className="font-sarabun">
+                <h2 className="mb-2 text-xl font-semibold text-black md:text-5xl">
+                  <span
+                    style={{ fontStyle: 'italic' }}
+                    className="font-roboto font-light"
+                  >
+                    Trigan
+                  </span>{' '}
+                  - solving social issues
+                </h2>
+
+                <p className="text-lg font-medium md:py-2 md:text-xl xl:px-36">
+                  Positive, fair solutions to poverty, corruption, inequality
+                  and deprivation using science and AI.
+                </p>
+
+                <div className="">
+                  <Image src={''} className="" alt="Image Goes here" />
+                </div>
+              </div>
+              <button className="rounded-full bg-primary px-8 py-4 font-medium text-slate-100">
+                Lorem ipsum
+              </button>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide className="w-full">
+            <div className="w-full">
+              <div className="font-sarabun">
+                <h2 className="mb-2 text-xl font-semibold text-black md:text-5xl">
+                  <span
+                    style={{ fontStyle: 'italic' }}
+                    className="font-roboto font-light"
+                  >
+                    Trigan
+                  </span>{' '}
+                  - Metaverse
+                </h2>
+
+                <p className="text-lg font-medium md:py-2 md:text-xl xl:px-36">
+                  A revolutionary local community-centric approach to medicine,
+                  workplace, education and social interaction.
+                </p>
+                <div className="">
+                  <Image src={''} className="" alt="Image Goes here" />
+                </div>
+              </div>
+              <button className="rounded-full bg-primary px-8 py-4 font-medium text-slate-100">
+                Lorem ipsum
+              </button>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     </header>
   )
