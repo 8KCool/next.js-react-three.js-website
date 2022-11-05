37f4bf613a8bc9a831a21fe3334dfc349d9cef14
andreyscott <andreyscott301@gmail.com>
2022-11-05 08:44:23 +0100

    fixed and optimise the ui for the blog-post

--- src/components/BlogHeader/BloGAccordionItems/index.tsx
+++ src/components/BlogHeader/BloGAccordionItems/index.tsx
@@ -1,18 +1,22 @@
 import { motion } from 'framer-motion'
 import { ReactNode, useState } from 'react'
-import { FaTimes } from 'react-icons/fa'
 import { BsChat } from 'react-icons/bs'
+import Link from 'next/link'
+import Image from 'next/image'
+
 interface FaqPanelProps {
   children?: ReactNode
   title: string
   content: string
   index: number
+  post: any
 }
 
 export const Blogpanel: React.FC<FaqPanelProps> = ({
   title,
   content,
   index,
+  post,
 }) => {
   const [showAnswer, setShowAnswer] = useState(false)
   return (
@@ -35,6 +39,25 @@ export const Blogpanel: React.FC<FaqPanelProps> = ({
         </>
       )}
       <div className="flex flex-col items-start justify-between w-full ">
+      {/* <div className="flex flex-col items-start w-full  h-auto"> */}
+                            <Link href={post.link}>
+                                <a className="blockmb-2 ">
+                                    <Image
+                                     className="object-cover w-full mb-2 overflow-hidden rounded-lg shadow-sm max-h-56" 
+                                     alt='blog-images' 
+                                    //  src={post.image}
+                                        src={showAnswer ? post.onclickImage : post.image}
+                                      width={500} height={300} />
+                                </a>
+                            </Link>
+                            <div className='flex py-1'>
+                                    {post.tags.tags.map((tag:string) => (
+                                        <div key={post.tags.id }
+                                              className="bg-purple-500 flex items-center px-3 py-1.5 leading-none rounded-full text-xs font-medium uppercase text-white">
+                                            <span>{tag}</span>
+                                            </div>
+                                    ))}
+                                    </div>
         <h3 
              className="text-lg py-2 font-bold sm:text-xl md:text-2xl"
           onClick={() => setShowAnswer(!showAnswer)}
