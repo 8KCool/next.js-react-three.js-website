dc299a3f33e6357dd00c9eb77c1e5f64e2047fe3
mahmoudT95 <mahmoud.t1995@gmail.com>
2022-11-16 02:23:41 +0200

    fixing nav on hr slid

--- src/components/home/HeroSection/HorizontalSlideShow.tsx
+++ src/components/home/HeroSection/HorizontalSlideShow.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import "swiper/css";
-import "swiper/css/pagination";
-import 'swiper/css/navigation';
-import { Pagination ,Navigation } from "swiper";
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { Pagination, Navigation } from 'swiper'
 const SlideShow = () => {
   const slidesContent = [
     'We have made a breakthrough in blockchain technology which enables us to tackle big global issues.',
@@ -15,14 +15,11 @@ const SlideShow = () => {
   return (
     <div className="relative py-[200px] px-4">
       <Swiper
-       slidesPerView={1}
-       spaceBetween={30}
-       loop={true}
-       pagination={{
-         clickable: true,
-       }}
-       navigation={true}
-       modules={[Pagination, Navigation]}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
         breakpoints={{
           // when window width is >= 640px
           320: {
@@ -46,7 +43,7 @@ const SlideShow = () => {
       >
         {slidesContent.map((slide, index) => (
           <SwiperSlide className="" key={slide}>
-            <div className=" horizontalCard relative  flex h-[200px] w-full max-w-[600px] items-center justify-center rounded-lg px-2 text-white font-semibold md2:px-10 md2:text-xl shadow-lg">
+            <div className=" horizontalCard relative  flex h-[200px] w-full max-w-[600px] items-center justify-center rounded-lg px-2 font-semibold text-white shadow-lg md2:px-10 md2:text-xl">
               <p className="text-center">{slide}</p>
             </div>
           </SwiperSlide>
