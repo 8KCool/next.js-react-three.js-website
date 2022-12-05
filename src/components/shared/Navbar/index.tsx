863409e3023829bccdd129922fbe59e65f6a94a8
MoizDoerz <moiz@doerz.tech>
2022-12-06 02:39:42 +0500

    header fix

--- src/components/shared/Navbar/index.tsx
+++ src/components/shared/Navbar/index.tsx
@@ -89,7 +89,7 @@ export const Navbar: React.FC<NavbarProps> = () => {
           }`}
         > */}
 
-      <nav className="max-w-screen  h-[80px] bg-black md:h-[128px]"
+      <nav className="max-w-screen  h-[80px] bg-red-500 md:h-[128px]"
       >
         {/* <div
           className={`top-0 left-0 z-10 w-full bg-transparent py-6 text-white md:px-0 ${
@@ -100,10 +100,9 @@ export const Navbar: React.FC<NavbarProps> = () => {
         > */}
         <div
           id="navbar"
-          className={`fixed top-0 left-0 z-30 w-full ${navBg ? 'bg-black' : 'bg-black'
-            } py-6 text-white transition-all md:px-0`}
+          className={`fixed top-0 left-0 z-30 w-full py-6 text-white transition-all md:px-0 border-b-2`}
         >
-          <div className="x-5 relative" style={{ backgroundColor: "black" }}>
+          <div className="x-5 relative" >
             <div className="flex items-center justify-around">
               {/* Logo And Title */}
               {/* <div
