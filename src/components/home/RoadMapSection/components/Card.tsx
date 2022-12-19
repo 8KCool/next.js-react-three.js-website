80784361a4978d12d2309124012288bdbd7ee60c
mdsifatul <mdsifatulislam@airasia.com>
2022-12-20 00:06:03 +0800

    fix: filename

--- src/components/home/RoadMapSection/components/Card.tsx
+++ src/components/home/RoadMapSection/components/Card.tsx
@@ -16,7 +16,7 @@ const getBackgroundInfo = (key=0) =>{
  
   
   return {
-    backgroundImage:'url(/images/tiger.jpeg)',
+    backgroundImage:'url(/images/img_5terre.jpeg)',
     backgroundSize:'100vw auto',
     backgroundPositionX,
     backgroundPositionY
