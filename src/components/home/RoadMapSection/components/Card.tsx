bbb8ddefba0d7021784d9123c4c81b90c96577a9
Abbas285 <syedabbasali.pak698@yahoo.com>
2022-12-24 23:18:36 +0500

    Show a flow colour on the existing line which has circles next to milestones (reaching the current milestone)
    Show a flow colour on the existing line which has circles next to milestones (reaching the current milestone)

--- src/components/home/RoadMapSection/components/Card.tsx
+++ src/components/home/RoadMapSection/components/Card.tsx
@@ -35,7 +35,9 @@ const Card = ({ date, rotate, imageUrl, name, index }: CardProps) => {
             rotate === true
               ? ' float-right ml-[2%] before:-left-[1px] before:-skew-y-[45deg] after:-left-[1px] after:skew-y-[45deg]'
               : ' float-left mr-[2%] before:-right-[1px] before:skew-y-[45deg] after:-right-[1px] after:-skew-y-[45deg]'
-          } post group relative w-[48%] overflow-hidden bg-[rgba(84,84,84,0.6)] py-[50px] text-center text-5xl uppercase text-white transition-all  duration-[0.5s] before:absolute before:bottom-[54%]  before:h-[55%] before:origin-[100%,0] before:bg-[#f2f2f2] before:pl-[3%]  before:content-[''] after:absolute after:top-[54%] after:h-[55%] after:origin-[100%,0] after:bg-[#f2f2f2] after:pl-[3%] after:content-[''] group-hover:!text-black`}
+          } post group relative w-[48%] overflow-hidden bg-[rgba(84,84,84,0.6)] py-[50px] text-center text-5xl uppercase text-white transition-all  duration-[0.5s]
+           before:absolute before:bottom-[54%]  before:h-[55%] before:origin-[100%,0] 
+           before:bg-[#f2f2f2] before:pl-[3%]  before:content-[''] after:absolute after:top-[54%] after:h-[55%] after:origin-[100%,0] after:bg-[#f2f2f2] after:pl-[3%] after:content-[''] group-hover:!text-black`}
         >
           <i
             style={{ backgroundImage: `url('${imageUrl}')` }}
@@ -49,10 +51,13 @@ const Card = ({ date, rotate, imageUrl, name, index }: CardProps) => {
           rotate === true
             ? 'before:-right-[10px] after:right-0'
             : 'before:-left-[10px] after:left-0'
-        }  relative float-left w-2/4 text-center before:absolute before:top-[63px]  before:z-10 before:h-5 before:w-5 before:rounded-full before:border-4  before:border-solid before:border-[#7d7d7d] before:bg-[#f2f2f2] before:shadow-[0_0_0_5px_rgba(250,250,250,0.9)] before:content-[''] after:absolute after:top-[73px] 
-         after:-z-[1px] after:h-[3px] after:w-2/4 after:bg-[#dddddd] after:content-['']`}
+        }  relative float-left w-2/4 text-center before:absolute before:top-[63px]  before:z-10 before:h-5 before:w-5 before:rounded-full before:border-4 
+         before:border-solid before:border-[#7d7d7d] 
+        before:bg-[#f2f2f2] before:shadow-[0_0_0_5px_rgba(250,250,250,0.9)] before:content-[''] after:absolute after:top-[73px] 
+         after:-z-[1px] after:h-[3px] after:w-2/4 after:bg-[#BBFF33] after:content-['']`}
       >
-        <div className={` subHeadStyle  ${rotate&&"mr-[79px]"} ${!rotate&&"ml-[79px]"} bg-transparent relative z-50 mt-[50px] inline-block border-2 border-solid border-[#7d7d7d] py-[10px] px-[30px] `}>
+        <div className={` subHeadStyle  ${rotate&&"mr-[79px]"} ${!rotate&&"ml-[79px]"} bg-transparent relative z-50 mt-[50px] inline-block border-2 border-solid
+         border-[#7d7d7d] py-[10px] px-[30px] `}>
           {date}
         </div>
       </div>
