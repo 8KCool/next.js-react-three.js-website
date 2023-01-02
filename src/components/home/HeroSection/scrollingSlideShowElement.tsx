1b751908a078aa65bd79580c5408525666816715
Titong0 <augustoerrecarte@gmail.com>
2023-01-02 20:10:23 -0300

    resize video and add learn more' buttons

--- src/components/home/HeroSection/scrollingSlideShowElement.tsx
+++ src/components/home/HeroSection/scrollingSlideShowElement.tsx
@@ -1,34 +1,38 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const ScrollingSlideShowElement = (props: any) => {
   const [active, setActive] = useState(false)
 
   return (
-    
     <div
-      className={`text-center cursor-pointer mb-[400px] text-white text-sm
-      ` 
-      //  ${
-      //   props.centered === props.index
-      //     ? 'bg-[#202635]   border-2 text-white md:w-[700px]'
-      //     : 'text-slate-400/60 md:w-[600px]'
-      // }
-      
-    }
-    data-aos="fade-up"
-    data-aos-anchor-placement="top-center"
-    id='slide-element'
+      className={
+        `mb-[400px] cursor-pointer text-center text-sm text-white
+      `
+        //  ${
+        //   props.centered === props.index
+        //     ? 'bg-[#202635]   border-2 text-white md:w-[700px]'
+        //     : 'text-slate-400/60 md:w-[600px]'
+        // }
+      }
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-center"
+      id="slide-element"
       ref={props.innerRef}
     >
       <h1
         className={`${
           props.centered === props.index ? '' : ''
         } font-m_plus_rounded_1c font-bold uppercase md:text-[60px] md2:text-[2vw] md2:leading-[1.2] md2:tracking-wider`}
-        style={{fontSize: '60px',lineHeight: '3.5rem'}}
+        style={{ fontSize: '60px', lineHeight: '3.5rem' }}
       >
         {props.headerText}
       </h1>
-      <p className="my-[30px] leading-8" style={{fontSize: '30px'}}>{props.mainText}</p>
+      <p className="my-[30px] leading-8" style={{ fontSize: '30px' }}>
+        {props.mainText}
+      </p>
+      <button className="mt-3 mb-4 w-36 rounded-full border bg-transparent py-2 px-4  font-m_plus_rounded_1c font-bold text-white hover:bg-gray-400">
+        Learn more
+      </button>
       {/* {props.buttons && (
         <div className="flex gap-4">
           {props.buttons.map((button: { text: string }) => (
