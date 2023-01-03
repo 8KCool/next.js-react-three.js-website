88535fa2274bc7df17cb3f2032ba043539407ea2
Titong0 <augustoerrecarte@gmail.com>
2023-01-02 22:04:33 -0300

    implement defaultShowDetails and redesign cards

--- src/components/Teams/TeamsByCategory/components/TeamCard.tsx
+++ src/components/Teams/TeamsByCategory/components/TeamCard.tsx
@@ -4,66 +4,71 @@ import { TeamMember } from '../../../../types/TeamMember'
 import { FadeInWhenVisible } from '../../../shared/FadeInWhenVisible'
 import { TeamSocialIcon } from '../../TeamSocialIcon'
 
+type TeamCardProps = {
+  teamMember: TeamMember
+  idx: number
+  defaultShowDetails: boolean
+}
 
-type Props = {}
-
-const TeamCard = ({teamMember,i}:any) => {
-    const [buttoClicked, setButtoClicked] = useState(false)
+const TeamCard: React.FC<TeamCardProps> = ({
+  teamMember,
+  idx,
+  defaultShowDetails,
+}) => {
+  const [buttonClicked, setButtonClicked] = useState(defaultShowDetails)
   return (
-    <FadeInWhenVisible duration={(i + 1) * 0.2} key={teamMember.id}>
-              <div
-                id={teamMember.id}
-                className="my-5 mx-5 bg-white dark:bg-light-grey md:mx-auto md:w-2/3 md:-skew-x-12"
-              >
-                <div className="grid items-center gap-5 overflow-hidden px-10 py-5 md:skew-x-12 md:grid-cols-4">
-                  {/* Image Starts */}
-                  <div className="relative mx-auto h-28 w-28 sm:h-40 sm:w-40 md:col-span-1">
-                    <Image
-                      src={teamMember.image}
-                      alt={teamMember.name}
-                      layout="fill"
-                      className="rounded-full bg-light-grey dark:bg-light"
-                    />
-                  </div>
-                  {/* Image Ends */}
+    <FadeInWhenVisible duration={(idx + 1) * 0.2} key={teamMember.id}>
+      <div
+        id={teamMember.id}
+        className="my-5 mx-3 bg-white dark:bg-light-grey md:-skew-x-12"
+      >
+        <div className="grid grid-cols-[80px_1fr] items-center gap-4 overflow-hidden px-10 py-5 md:skew-x-12">
+          {/* Image Starts */}
+          <div className="relative aspect-1 w-full">
+            <Image
+              src={teamMember.image}
+              alt={teamMember.name}
+              layout="fill"
+              className="rounded-full bg-light-grey dark:bg-light"
+            />
+          </div>
+          {/* Image Ends */}
 
-                  <div className="container col-span-3 pt-3">
-                    <div>
-                      <h4 className="text-xl font-semibold">
-                        {teamMember.name}
-                      </h4>
-                      <h6 className="font-medium">{teamMember.title}</h6>
-                    </div>
+          <div className="col-start-2 col-end-4 pt-3">
+            <div>
+              <h4 className="text-xl font-semibold">{teamMember.name}</h4>
+              <h6 className="font-medium">{teamMember.title}</h6>
+            </div>
 
-                    {buttoClicked ? (
-                      <div>
-                        <p className="py-2 text-sm">
-                          {teamMember.longDescription}
-                        </p>
-                      </div>
-                    ) : null}
-                    <div className="flex justify-between">
-                      <div>
-                      <button
-                        onClick={() => {
-                          setButtoClicked(!buttoClicked)
-                        }}
-                        className={`group relative mt-4  mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-green-400 to-blue-600 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-200 group-hover:from-green-400 group-hover:to-blue-600 dark:text-white dark:focus:ring-green-800`}
-                      >
-                        <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
-                          {buttoClicked ? 'Hide' : 'See Details'}
-                        </span>
-                      </button>
-                      </div>
-                      <div> {
-                        buttoClicked? <TeamSocialIcon teamMember={teamMember} />:null
-                      }</div>
-                     
-                    </div>
-                  </div>
-                </div>
+            {buttonClicked ? (
+              <div>
+                <p className="py-2 text-sm">{teamMember.longDescription}</p>
               </div>
-            </FadeInWhenVisible>
+            ) : null}
+            <div className="flex justify-between">
+              <div>
+                <button
+                  onClick={() => {
+                    setButtonClicked(!buttonClicked)
+                  }}
+                  className={`group relative mt-4  mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-green-400 to-blue-600 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-green-200 group-hover:from-green-400 group-hover:to-blue-600 dark:text-white dark:focus:ring-green-800`}
+                >
+                  <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
+                    {buttonClicked ? 'Hide' : 'See Details'}
+                  </span>
+                </button>
+              </div>
+              <div>
+                {' '}
+                {buttonClicked ? (
+                  <TeamSocialIcon teamMember={teamMember} />
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </FadeInWhenVisible>
   )
 }
 
--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,59 +1,56 @@
-import Image from "next/image";
-import Link from "next/link";
-import {
-  FaFacebookSquare,
-  FaLinkedin,
-} from 'react-icons/fa'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FaFacebookSquare, FaLinkedin } from 'react-icons/fa'
 
 const teams = [
   {
-    job: "Chief Executive Officer & Chief Technology Officer",
-    name: "Aaron Sarginson",
+    job: 'Chief Executive Officer & Chief Technology Officer',
+    name: 'Aaron Sarginson',
     imageSrc:
-      "https://uploads-ssl.webflow.com/62b42d68bf74cf04cd20f5c7/62b510183218d5e6f542a017_Aaron.png",
-    imageAlt: "Aaron Sarginson.",
+      'https://uploads-ssl.webflow.com/62b42d68bf74cf04cd20f5c7/62b510183218d5e6f542a017_Aaron.png',
+    imageAlt: 'Aaron Sarginson.',
     description:
-      "Aaron is a technologist, believing that technology can solve many of the problems affecting humanity today. An innovator with a keen interest in building as big as he talks, Aaron is the conceptual founder and main driving force behind Trigan and the Smartest City project. He has a Computer Science degree and experience in software and hardware architecture, embedded systems design, programming, project management, business leadership, web development, network administration and systems administration.",
+      'Aaron is a technologist, believing that technology can solve many of the problems affecting humanity today. An innovator with a keen interest in building as big as he talks, Aaron is the conceptual founder and main driving force behind Trigan and the Smartest City project. He has a Computer Science degree and experience in software and hardware architecture, embedded systems design, programming, project management, business leadership, web development, network administration and systems administration.',
     socials: [
-      { facebook: "https://www.facebook.com/Aaron.Thomas.Sarginson" },
-      { linkedin: "https://www.linkedin.com/in/aaronsarginson" }
+      { facebook: 'https://www.facebook.com/Aaron.Thomas.Sarginson' },
+      { linkedin: 'https://www.linkedin.com/in/aaronsarginson' },
     ],
   },
 
   {
-    job: "Chief Operating Officer",
-    name: "Dr. Gunel Sarginson",
+    job: 'Chief Operating Officer',
+    name: 'Dr. Gunel Sarginson',
     imageSrc:
-      "https://uploads-ssl.webflow.com/62b42d68bf74cf04cd20f5c7/62b51018ed3ca312442ebb44_Gunel.png",
-    imageAlt: "Dr. Gunel Sarginson",
+      'https://uploads-ssl.webflow.com/62b42d68bf74cf04cd20f5c7/62b51018ed3ca312442ebb44_Gunel.png',
+    imageAlt: 'Dr. Gunel Sarginson',
     description:
-      "Gunel is very interested in how our solution can help solve social and medical challenges faced by communities today. While she is a Medical Doctor and Social Scientist, she also has a wide and varied background in marketing and brand leadership at major corporations in multiple countries, including the UK. Gunel has held related board positions which she has given up for Trigan and has undertaken Public Health research for the NHS. She has strong entrepreneurial experience in multiple sectors. Gunel is extremely excited about our vision at Trigan and how we can truly change the world for the better.",
+      'Gunel is very interested in how our solution can help solve social and medical challenges faced by communities today. While she is a Medical Doctor and Social Scientist, she also has a wide and varied background in marketing and brand leadership at major corporations in multiple countries, including the UK. Gunel has held related board positions which she has given up for Trigan and has undertaken Public Health research for the NHS. She has strong entrepreneurial experience in multiple sectors. Gunel is extremely excited about our vision at Trigan and how we can truly change the world for the better.',
     socials: [
-      { facebook: "" },
-      { linkedin: "https://www.linkedin.com/in/gunelsarginson" }
+      { facebook: '' },
+      { linkedin: 'https://www.linkedin.com/in/gunelsarginson' },
     ],
   },
-];
+]
 
 const AboutComponent = () => {
   return (
     <>
-
       <section className="overflow-hidden bg-transparent">
-      <div className=" text-white pb-2 text-5xl font-semibold text-center title">
-<h1>Leadership</h1></div>
+        <div className="title pb-2 text-center text-5xl font-semibold text-white">
+          <h1>Leadership</h1>
+        </div>
         {/* TEAM CARD SECTION */}
         <div className="my-[4%] ">
           {teams.map((team) => (
             //  < !--container for all cards -->
             <div
               key={team.name}
-              className="container w-100 lg:w-4/5 mx-auto flex flex-col "
+              className="w-100 container mx-auto flex flex-col lg:w-4/5 "
             >
               {/* <!-- card -->  */}
-              <div className="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2 bg-gradient-to-br from-white to-slate-200">
+              <div className="w-100 mx-2 mt-4 flex flex-col overflow-hidden rounded-lg bg-white bg-gradient-to-br from-white to-slate-200 shadow-xl md:flex-row">
                 {/* <!-- media --> */}
-                <div className="h-64 flex justify-center my-5 w-auto md:w-1/2">
+                <div className="my-5 flex h-64 w-auto justify-center md:w-1/2">
                   <img
                     className="lg:rounded-full"
                     src={team.imageSrc}
@@ -61,24 +58,32 @@ const AboutComponent = () => {
                   />
                 </div>
                 {/* <!-- content --> */}
-                <div className="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
-                  <h3 className="pb-0 text-transparent bg-clip-text bg-gradient-to-br from-purple-800 to-rose-500 font-m_plus_rounded_1c font-semibold text-lg leading-tight truncate">
+                <div className="flex w-full flex-col justify-between py-4 px-6 text-gray-800">
+                  <h3 className="truncate bg-gradient-to-br from-purple-800 to-rose-500 bg-clip-text pb-0 font-m_plus_rounded_1c text-lg font-semibold leading-tight text-transparent">
                     {team.name}
                   </h3>
-                  <p className="text-base font-m_plus_rounded_1c text-black">{team.job}</p>
-                  <p className="mt-2 text-gray-600 font-m_plus_rounded_1c">{team.description}</p>
-                  <p className="text-sm text-gray-700 font-m_plus_rounded_1c uppercase tracking-wide font-semibold mt-2">
+                  <p className="font-m_plus_rounded_1c text-base text-black">
+                    {team.job}
+                  </p>
+                  <p className="mt-2 font-m_plus_rounded_1c text-gray-600">
+                    {team.description}
+                  </p>
+                  <p className="mt-2 font-m_plus_rounded_1c text-sm font-semibold uppercase tracking-wide text-gray-700">
                     {/* {{ card.author }} &bull; {{ card.date }} */}
                   </p>
                   <div className="relative flex space-x-5">
                     {team.socials?.map((social, index) => (
-                      <div key={index} >
-                        {social.facebook ? <a href={social.facebook} >
-                          <FaFacebookSquare className="text-2xl text-sky-700" />
-                        </a> : null}
-                        {social.linkedin ? <a href={social.linkedin}>
-                          <FaLinkedin className="text-2xl text-sky-800" />
-                        </a> : null}
+                      <div key={index}>
+                        {social.facebook ? (
+                          <a href={social.facebook}>
+                            <FaFacebookSquare className="text-2xl text-sky-700" />
+                          </a>
+                        ) : null}
+                        {social.linkedin ? (
+                          <a href={social.linkedin}>
+                            <FaLinkedin className="text-2xl text-sky-800" />
+                          </a>
+                        ) : null}
                       </div>
                     ))}
                   </div>
@@ -89,7 +94,7 @@ const AboutComponent = () => {
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default AboutComponent;
+export default AboutComponent
