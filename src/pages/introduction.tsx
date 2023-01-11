1c17ef11a2fd8c61acaf13efc794169a50ed5a7c
rifat-shaun <rifath.shaun@gmail.com>
2023-01-11 22:04:46 +0600

    2nd page designwithout responsive behaviour

--- src/pages/introduction.tsx
+++ src/pages/introduction.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import { GlobalLayout } from '../components/layouts/GlobalLayout';
 import { SEO } from '../components/shared/SEO';
