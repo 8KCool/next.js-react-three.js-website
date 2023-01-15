696d056e29721c8a59d6840070e6a1b25504aeb2
unknown <giri@10000>
2023-01-15 10:55:31 +0300

    Three Js Animation is more optimzed

--- public/EarthTexture/Static.js
+++ public/EarthTexture/Static.js
@@ -1,6 +1,7 @@
 import React, { memo, useEffect, useRef, useState } from 'react'
 import { useAnimations, useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
 
 // Model of Earth that does not change on scroll
 const Model = (props) => {
@@ -10,6 +11,7 @@ const Model = (props) => {
   const [Device, setDevice] = useState(30)
   const earthRef = useRef()
   const cloudRef = useRef()
+  const [lod, setLod] = useState("medium")
 
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime()
@@ -22,8 +24,19 @@ const Model = (props) => {
     } else {
       setDevice(33)
     }
+    if (window.innerWidth > 1920) {
+      setLod("high")
+    } else if (window.innerWidth > 1024) {
+      setLod("medium")
+    } else {
+      setLod("low")
+    }
   }, [setDevice, window.innerWidth])
 
+
+  materials.Earth.emissive = new THREE.Color(0x336699)
+  materials.Clouds.transparent = true;
+  materials.Clouds.opacity = 0.5;
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
@@ -33,17 +46,34 @@ const Model = (props) => {
           ref={cloudRef}
         >
           <group scale={96.72} ref={earthRef}>
-            <mesh geometry={nodes.Earth.geometry} material={materials.Earth} />
+            {lod === "high" ? <mesh geometry={nodes.Earth.geometry} material={materials.Earth} /> : null}
+            {lod === "medium" ? <mesh geometry={nodes.Earth.geometry} material={materials.Earth} /> : null}
+            {lod === "low" ? <mesh geometry={nodes.Earth.geometry} material={materials.Earth} /> : null}
           </group>
-          <group ref={earthRef} scale={97.46}>
+          <group scale={[0.5, 0.5, 0.5]} ref={earthRef}>
+            
             <mesh
               geometry={nodes.Clouds.geometry}
               material={materials.Clouds}
             />
           </group>
+          <mesh
+            scale={[0.5, 0.5, 0.5]}
+            position={[20, 20, 20]}
+            geometry={nodes.Clouds.geometry}
+            material={materials.Clouds}
+          />
+          <mesh
+            scale={[0.3, 0.3, 0.3]}
+            position={[-20, -20, -20]}
+            rotation={[0, Math.PI / 2, 0]}
+            geometry={nodes.Clouds.geometry}
+            material={materials.Clouds}
+          />
+          </group>
         </group>
       </group>
-    </group>
+    
   )
 }
 
