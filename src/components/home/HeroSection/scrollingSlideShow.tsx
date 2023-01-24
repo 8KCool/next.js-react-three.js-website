50e5feccd9679f89510810e300bf23fbf719816a
mostafa khedre <moustafa.khedre@rowaad.net>
2023-01-24 04:01:07 +0200

    fix

--- src/components/home/HeroSection/scrollingSlideShow.tsx
+++ src/components/home/HeroSection/scrollingSlideShow.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect,createRef,useRef,useState,} from 'react'
+import React, { useEffect, createRef, useRef, useState } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { FadeInWhenVisible } from '../../shared/FadeInWhenVisible'
@@ -17,22 +17,36 @@ const ScrollingSlideShow = () => {
         <FadeInWhenVisible>
           <div>
             {/* Video Starts */}
-              <h6 className='intro_h' style={{"font-size":"40px","padding":"25px 160px"}as React.CSSProperties}>An Introduction <span style={{"color":"#A855F7"}}>to Trigan</span></h6>
-            <div className="aspect-h-9 aspect-w-16  flex w-full justify-center opacity-100 first-line:relative ">
+            <h6
+              className="intro_h"
+              style={
+                {
+                  'font-size': '40px',
+                  padding: '25px 160px',
+                } as React.CSSProperties
+              }
+            >
+              An Introduction{' '}
+              <span style={{ color: '#A855F7' }}>to Trigan</span>
+            </h6>
+            <div className="flex justify-center w-full opacity-100 aspect-h-9 aspect-w-16 first-line:relative ">
               {/* <AnimationBlob /> */}
-              <div className="mx-auto h-3/4 w-3/4 shadow-xl shadow-black">
+              <div className="w-3/4 mx-auto shadow-xl h-3/4 shadow-black">
                 <iframe
                   allowFullScreen
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  className="h-full w-full object-cover"
+                  className="object-cover w-full h-full"
                   src="https://www.youtube.com/embed/VY-QKe19p0Y?mode=opaque&amp;rel=0&amp;autohide=1&amp;showinfo=0&amp;wmode=transparent"
                 />
               </div>
             </div>
             {/* /* Video Ends */}
-            <div className="mx-auto mb-30 max-w-6xl py-5 text-center font-extralight text-slate-100">
-              <h2 className="headingStyle relative mt-10 mb-10 text-xl text-white md:text-5xl">
-                Dream of a <span style={{"color":"#A855F7"}as React.CSSProperties}>better future.</span>
+            <div className="max-w-6xl py-5 mx-auto text-center mb-30 font-extralight text-slate-100">
+              <h2 className="relative mt-10 mb-10 text-xl text-white headingStyle md:text-5xl">
+                Dream of a{' '}
+                <span style={{ color: '#A855F7' } as React.CSSProperties}>
+                  better future.
+                </span>
               </h2>
               <HorizontalSlider />
             </div>
